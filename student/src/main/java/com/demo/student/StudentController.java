8963e8f0f314489e6fa1ec03d541d370e30f52cf
Mountassir <mb.bouyebla@gmail.com>
2022-11-06 17:29:46 +0100

    fixed folder structure and changed branch

--- student/src/main/java/com/demo/student/StudentController.java
+++ student/src/main/java/com/demo/student/StudentController.java
@@ -1,10 +1,7 @@
-package com.example.demo;
+package com.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/students/")
